6c8745bae13447ad933cd5c50f25269e57916135
王内涵 <wangxiangdong@wangxiaongdembp.lan>
2018-08-13 17:49:24 +0800

    123

--- pzsh/AppDelegate.m
+++ pzsh/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //2131432
     return YES;
 }
 
